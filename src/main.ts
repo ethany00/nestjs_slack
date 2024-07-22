9342c6e65186231776163af5925bb419422fd660
Ethan <Ethan@yoob.local>
2024-07-22 22:22:03 +0900

    기본 세팅 및 핫리로딩 설정.

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as process from "node:process";
+declare const module: any;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`listening to ${port}`);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+
+}
+bootstrap();
